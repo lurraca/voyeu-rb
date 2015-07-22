1a0cd1ffd4a1e05c21b77e49dfb0c03475200f3f
Luis Urraca <l.urraca@gmail.com>
2015-07-22 23:13:04 +0100

    Added gems to spec_helper. configured VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'voyeur'
+
+require 'webmock/rspec'
+require 'vcr'
+
+require 'pry'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures"
+  c.hook_into :webmock
+end
